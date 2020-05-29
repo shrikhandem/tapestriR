6a50d76c3b53fa914f6c4c67705eb532e0d2d1bd
Anup <parikh@missionbio.com>
2020-05-28 21:13:23 -0700

    start to heatmap

--- R/visualization.R
+++ R/visualization.R
@@ -135,3 +135,81 @@ recode_genotypes <- function(x,collapse_zygosity=TRUE) {
     
   }
 }
+
+
+#' Plot heatmap
+#'
+#' @param analytes multi assay object
+#' @param cluster cluster labels
+#' @param snv_features snv features
+#' @param prot_features prot features
+#' @param groupby group by gene/chr and select variant with highest mutation rate
+#' @return heatmap object
+#' @export
+#' @examples
+#' \dontrun{
+#' p <- multiassay_heatmap(analytes, cluster, snv_features=c("DNMT3A", "JAK2"),
+#'    prot_features=c("CD3", "CD34"), groupby="gene")
+#' }
+multiassay_heatmap <- function(moo, cluster) {
+  clazz <- class(analytes)[1]
+  if (!(clazz %in% c("MultiAssay", "TapestriR"))) {
+    stop("Please specify multi assay object")
+  }
+  
+  if (!is.null(groupby) && groupby != "gene") {
+    stop("Please provide valid groupby option.")
+  }
+  
+  snv <- filter_slots_by_type(analytes, 'snv')
+  prot <- filter_slots_by_type(analytes, 'prot')
+  
+  snv.h <- NULL
+  protein.h <- NULL
+  
+  if (!is.null(snv)) {
+    genotypes <- snv$data
+    
+    #genotype <- genotypes[, snv_features]
+    
+    genotypes.mat <- genotype_matrix(genotypes, groupby)
+    
+    #genotypes.mat <- genotypes.mat[order(match(rownames(genotypes.mat), snv@clones$Cell)), ]
+    
+    legend_params <- list(title = "Genotype", at = c(0, 1, 2, 3),
+                          border = "black", labels = c("WT", "HET", "HOM", "Missing"), 
+                          title_gp = grid::gpar(fontsize = 10, fontface = "bold"), 
+                          labels_gp = grid::gpar(fontsize = 7), legend_height = 5, legend_width = 5, 
+                          grid_height = grid::unit(5, "mm"), grid_width = grid::unit(5, "mm"))
+    col_fun <- c("grey", "red", "darkred", "white")
+    
+    if (!(is.null(snv_features))) {
+      if (is.null(groupby)) {
+        snv_features <- tapestri.cnv.loh.sort_by_loc(snv_features)
+      }
+      genotypes.mat <- genotypes.mat[, snv_features]
+    }
+    
+    snv.h <- ComplexHeatmap::Heatmap(as.matrix(genotypes.mat), name = "GT",
+                                     column_order = colnames(genotypes.mat), split=factor(cluster),
+                                     show_row_names=FALSE, row_title_gp = grid::gpar(fontsize = 6),
+                                     col=col_fun, heatmap_legend_param=legend_params,
+                                     column_names_gp = grid::gpar(fontsize=8),
+                                     show_column_dend=FALSE)
+  }
+  
+  if (!is.null(prot)) {
+    protein <- prot$data
+    if (!is.null(prot_features)) {
+      protein <- protein[, prot_features]
+    }
+    
+    protein.h <- ComplexHeatmap::Heatmap(as.matrix(protein), name = "Normalized Protein Counts", 
+                                         split=factor(cluster), show_row_names=FALSE,  row_title_gp = grid::gpar(fontsize = 5), 
+                                         column_names_gp = grid::gpar(fontsize=8))
+  }
+  
+  return(protein.h + snv.h)
+}
+
+
